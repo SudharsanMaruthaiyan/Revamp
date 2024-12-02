39f597d2789abfab0cafbfbfed422ad7a8cd45d4
Honey-Joe <honeyjoe942@gmail.com>
2024-12-02 16:08:18 +0530

    uodated in mobile navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -76,10 +76,16 @@ const Navbar = () => {
                       </Link>
                     </li>
                     <li>
+                      <Link to={"/blog"}>
+                      
                       <a href="#venue" className=" font-[poppins] font-medium text-lg">Blog</a>
+                      </Link>
                     </li>
                     <li>
+                      <Link to={"/contact"}>
+                      
                       <a href="#venue" className=" font-[poppins] font-medium text-lg">Contact</a>
+                      </Link>
                     </li>
                     <li className="">
                       <button className=" font-[poppins] bg-white hover:bg-[#FF9F00] hover:border-white border-2 py-2 px-4 rounded-md  text-white font-medium text-lg">Login</button>
