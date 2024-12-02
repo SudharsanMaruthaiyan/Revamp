c334d1b3886ebc903649788eaf3ba371ee83e7d3
Honey-Joe <honeyjoe942@gmail.com>
2024-12-02 20:46:18 +0530

    blog section updated

--- src/Components/Blog2/Blog2.jsx
+++ src/Components/Blog2/Blog2.jsx
@@ -6,56 +6,31 @@ const Blog2 = () => {
   return (
     <>
       <div className="max-w-[100%]">
-        <div className="w-[80%] mx-auto grid grid-cols-1 pb-20">
-          <div className=" flex items-center justify-between">
-            <div className="flex justify-center flex-col items-start">
-              <div className="">
-                <h5 class="flex gap-3">
-                      <p className="font-[Poppins] text-[18px] md:text-[22px] lg:text-[24px] font-semibold text-[#FF9F00] ">
-                        Latest Blog
-                      </p>
-                      <svg
-                        class="arrow-svg"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 55 13"
-                        className="w-[60px] fill-[#F57104]"
-                      >
-                        <g clip-path="url(#clip0_324_36194)">
-                          <path d="M10.5406 6.49995L0.700562 12.1799V8.56995L4.29056 6.49995L0.700562 4.42995V0.819946L10.5406 6.49995Z"></path>
-                          <path d="M25.1706 6.49995L15.3306 12.1799V8.56995L18.9206 6.49995L15.3306 4.42995V0.819946L25.1706 6.49995Z"></path>
-                          <path d="M39.7906 6.49995L29.9506 12.1799V8.56995L33.5406 6.49995L29.9506 4.42995V0.819946L39.7906 6.49995Z"></path>
-                          <path d="M54.4206 6.49995L44.5806 12.1799V8.56995L48.1706 6.49995L44.5806 4.42995V0.819946L54.4206 6.49995Z"></path>
-                        </g>
-                      </svg>
-                </h5>
-              </div>
-              <div className="flex justify-center pb-9">
-                <p className="font-[Poppins] text-[22px]  md:text-[32px] lg:text-[40px] font-semibold">Latest <span className=" underline text-[#FF9F00]">Updates & Articles</span></p>
-              </div>
+        <div className="lg:w-[80%] w-[90%] mx-auto grid grid-cols-1 pb-20">
+          <div className="flex justify-center flex-col items-center gap-3 py-5 w-[60%] mx-auto">
+            <div>
+              <p className="font-[Poppins] text-[#FF9F00]">Articles</p>
             </div>
             <div>
-                <p className=' font-[poppins] font-semibold text-lg bg-[#FF9F00] text-white transition-colors py-2 px-5 rounded-lg '>Explore All Courses</p>
+              <h1 className="lg:text-[48px] text-[24px] font-[Poppins] font-bold">Our Blog Updates</h1>
+            </div>
+            <div>
+              <p className="text-center text-[16px] font-[Poppins]">Seuris finibus eleifend ante quis scelerisque. Nunc rutrum eget dui quis pulvinar. Curabitur et tincidunt lectus, gravida elementum ligula</p>
             </div>
           </div>
-          
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 gap-y-24">
-            {
-                Blog1Data.map((e)=>{
-                    return(
-                        <Blog1card
-                            imgurl = {e.imgurl}
-                            tag = {e.tag}
-                            designation = {e.designation}
-                            date = {e.date}
-                            authorimg = {e.authorimg}
-                            blogtitle = {e.blogtitle}
-                            authorname= {e.authorname}
-                        >
-                        </Blog1card>
-                    )
-                })
-            }
-             
+
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-7 gap-y-24">
+            {Blog1Data.map((e) => {
+              return (
+                <Blog1card
+                  imgurl={e.imgurl}
+                  tag={e.tag}
+                  desc={e.desc}
+                  comment={e.comment}
+                  blogtitle={e.blogtitle}
+                ></Blog1card>
+              );
+            })}
           </div>
         </div>
       </div>
--- src/Components/Blog2/Blog2card.jsx
+++ src/Components/Blog2/Blog2card.jsx
@@ -1,57 +1,33 @@
-import { ArrowRight, ChevronRight, Plus } from "lucide-react";
+import { ArrowRight, ChevronRight, Plus, RectangleEllipsisIcon, User } from "lucide-react";
 
 const Blog2card = (props) => {
   return (
     <>
-    <div className=" relative" id="shadow">
-      <div className="overflow-hidden group relative hover:-translate-y-2 transition-all">
-        <img src={props.imgurl} alt="blog image" className="w-[100%] h-[400px]  object-cover z-20 rounded-t-xl" />
-        <div className="absolute top-0 w-full h-full flex justify-center items-center -translate-y-[100%] group-hover:-translate-y-[1%] transition  bg-slate-300 opacity-55 rounded-xl">
-            <Plus className=""></Plus>
+      <div className="grid grid-cols-1 lg:grid-cols-7 gap-5 p-5 border rounded-xl border-gray-500">
+        <div className="lg:col-span-2 w-full h-full">
+            <img src={props.imgurl} alt=""  className="lg:w-[200px] lg:h-[220px] object-cover hover:scale-105 transition rounded-lg"/>
         </div>
-      </div>
-      <div className="rounded-xl flex flex-col shadow-lg  transition mx-auto absolute right-4 left-4 -bottom-16 bg-white ">
-        <div className="flex flex-col p-5 gap-5">
-          <div className="flex justify-between">
+        <div className="lg:col-span-5 flex flex-col gap-4">
+          <div className="flex gap-7">
             <div>
-              <button className="px-3 text-[14px] rounded-md py-0.5 bg-[#FF9F00] font-[Poppins] text-white hover:bg-black ">{props.tag}</button>
+              <p className="flex gap-2 font-[Poppins] items-center"><User></User> {props.tag}</p>
             </div>
             <div>
-              <p className="font-[Poppins] text-[16px] font-medium text-[#FF9F00] ">{props.date}</p>
+              <p className="flex gap-2 font-[Poppins] items-center"><RectangleEllipsisIcon></RectangleEllipsisIcon> {props.comment} Comments</p>
             </div>
           </div>
           <div>
-            <p className="font-[Poppins] text-[16px] md:text-[18px] lg:text-[24px] text-black transition-colors hover:text-[#FF9F00] font-medium">{props.blogtitle}</p>
+            <p className="font-[Poppins] font-bold text-[20px] hover:text-[#FF9F00] lg:text-[25px]">{props.blogtitle}</p>
           </div>
-          <div className="flex justify-between">
-            <div className="flex gap-2 items-center">
-              <div>
-                <img src={props.authorimg} alt="" className=" w-12 rounded-full"/>
-              </div>
-              <div>
-                <div>
-                  <p className="font-[Poppins] text-[16px] font-medium text-[#2a254d] hover:text-[#FF9F00] cursor-pointer">{props.authorname}</p>
-                  <p className="font-[Poppins] text-[12px] font-medium text-[#2a254d]">{props.designation}</p>
-                </div>
-              </div>
-            </div>
-            <div>
-              <a
-                href="#_"
-                class="relative inline-flex items-center justify-center px-5 py-5 overflow-hidden font-medium transition duration-300 ease-out border-2 border-[#F57104] rounded-full shadow-md group"
-              >
-                <span class="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-[#F57104] group-hover:translate-x-0 ease">
-                  <ArrowRight></ArrowRight>
-                </span>
-                <span class="absolute flex items-center justify-center w-full h-full transition-all duration-300 transform group-hover:translate-x-full ease">
-                <ArrowRight></ArrowRight>
-                </span>
-              </a>
-            </div>
+          <div>
+            <p className="font-[Poppins] text-[14px] ">{props.desc}</p>
+          </div>
+          <div>
+            <button className="font-[Poppins] text-[16px] flex gap-2 items-center">Read More <ArrowRight></ArrowRight></button>
           </div>
         </div>
+
       </div>
-    </div>
     
     
     </>
